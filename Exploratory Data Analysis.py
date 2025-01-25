cc9b2fcdbc3f51dc279ff53dce10ca33e6c7883b
Nikheel Patil <142206774+nikheelpatil17@users.noreply.github.com>
2025-01-25 10:17:42 +0530

    Update Exploratory Data Analysis.py

--- Exploratory Data Analysis.py
+++ Exploratory Data Analysis.py
@@ -1,3 +1,7 @@
+# This project focuses on conducting a thorough Exploratory Data Analysis (EDA) on Titanic Dataset.
+# The primary objective is to gain a deep understanding of the dataset's characteristics, identify key trends, patterns, and relationships within the data, and uncover valuable insights that can inform further analysis or decision-making.
+# Key Activities: Data Loading, Cleaning Data, Exploration & Visualization
+
 import pandas as pd 
 import matplotlib.pyplot as plt
 import seaborn as sns
